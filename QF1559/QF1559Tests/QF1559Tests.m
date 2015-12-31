048b49339ec23adeaf3d2ee28e3592e3349de8a6
JCHSS <136691850@qq.com>
2015-12-31 09:56:23 +0800

    这个是我们第一次提交

---
+++ QF1559/QF1559Tests/QF1559Tests.m
@@ -0,0 +1,40 @@
+//
+//  QF1559Tests.m
+//  QF1559Tests
+//
+//  Created by MS on 15-12-31.
+//  Copyright (c) 2015年 MS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface QF1559Tests : XCTestCase
+
+@end
+
+@implementation QF1559Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
